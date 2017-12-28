a22f31250f5d93628380a12c1af8c65f67df120b
Ahmed Jazzar <me@ahmedjazzar.com>
2017-12-28 19:08:14 +0200

    Pop up the version to 1.1.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='djangomako',
-    version='1.0.1',
+    version='1.1.0',
     packages=['djangomako'],
     install_requires=['Mako==1.0.7'],
     classifiers=[
