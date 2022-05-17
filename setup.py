abe30a67708181a714e08186e0e3e4b54ef40a64
Ahmed Jazzar <11036472+ahmedaljazzar@users.noreply.github.com>
2022-05-17 16:53:23 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,31 +1,36 @@
 #!/usr/bin/env python
+from pathlib import Path
+
 from setuptools import setup
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="djangomako",
-    version="1.2.1",
+    version="1.3.0",
     packages=["djangomako"],
-    install_requires=["Mako==1.0.7"],
+    install_requires=["Mako==1.2.0"],
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
         "Operating System :: OS Independent",
-        "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.2",
-        "Programming Language :: Python :: 3.3",
-        "Programming Language :: Python :: 3.4",
-        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
         "Framework :: Django",
-        "Framework :: Django :: 2.1",
-        "Framework :: Django :: 1.11",
+        "Framework :: Django :: 3.2",
+        "Framework :: Django :: 4.0",
         "License :: OSI Approved :: MIT License",
         "Topic :: Software Development",
     ],
-    url="https://github.com/ahmedaljazzar/django-mako",
+    url="https://github.com/iamjazzar/django-mako",
     license="MIT License",
     author="Ahmed Jazzar",
     author_email="me@ahmedjazzar.com",
     description="The simple, elegant Django Mako library",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
 )
